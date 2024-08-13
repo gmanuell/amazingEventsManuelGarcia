c7d74ec826fde0acd82943a94be69ae1fd475733
Manuel Garcia <manuelgarcia8295@gmail.com>
2024-08-13 07:39:48 -0500

    listo

--- script/details.js
+++ script/details.js
@@ -198,18 +198,23 @@ const data = {
   
 const url = window.location.search;
 const urlObjeto = new URLSearchParams(url);
-
-const eventId = urlObjeto.get('id')
+const eventId = urlObjeto.get('id');
 
 if (eventId) {
     const event = data.events.find(e => e._id === eventId);
-    console.log(event);
-
-
-    if (event) {
+    
+     if (event) {
         const cardContainer = document.getElementById("card-container"); 
         const cardContent = document.createElement("div");
         cardContent.className = "d-flex align-items-center ";
+
+        const eventDate = new Date(event.date);
+        const currentDate = new Date(data.currentDate);
+
+        const attendanceOrEstimate = eventDate < currentDate ? 
+            `<p class="assistance d-card">Assistance: ${event.assistance ? event.assistance : 'N/A'}</p>` :
+            `<p class="estimate d-card">Estimate: ${event.estimate ? event.estimate : 'N/A'}</p>`;
+
 
         cardContent.innerHTML = `
         <div class="cardDetails align-items-center col-12 border flex-column border-dark flex-sm-column d-flex flex-wrap flex-lg-row">
@@ -224,7 +229,7 @@ if (eventId) {
                     <p class="category d-card">${event.category}</p>
                     <p class="place d-card">${event.place}</p>
                     <p class="capacity d-card">Capacity: ${event.capacity}</p>
-                    <p class="estimate d-card">Estimate: ${event.estimate ? event.estimate : 'N/A'}</p>
+                    ${attendanceOrEstimate}
                     <p class="price d-card">Price: ${event.price} $</p>
                 </div>
             </div>
